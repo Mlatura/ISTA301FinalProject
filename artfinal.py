74ffba32df5b07ce68639c4c95eb67f00ef89541
Matthew Latura <MLatura@gmail.com>
2013-11-21 00:33:35 -0700

    Simple collage demo

--- artfinal.py
+++ artfinal.py
@@ -12,30 +12,12 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class CanvasImage(ndb.Model):
-    """Models an image created and submitted by a user"""
-    author = ndb.UserProperty()
-    imageBlob = ndb.BlobProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
-
-class ImageCreator(webapp2.RequestHandler):
-
+class Collage(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        if user:
-            template_values = {
-                "username": user.nickname(),
-                "logout_url": users.create_logout_url(self.request.uri)
-            }
-            template = JINJA_ENVIRONMENT.get_template('imageCreator.html')
-            self.response.write(template.render(template_values))
-        else:
-            self.redirect(users.create_login_url(self.request.uri)) 
-
-#class Collage(webapp2.RequestHandler):
+        template = JINJA_ENVIRONMENT.get_template('collage.html')
+        self.response.write(template.render())
 
 
 application = webapp2.WSGIApplication([
-    ('/', ImageCreator)
-#    ('/collage', Collage),
+    ('/', Collage)
 ], debug=True)
---
+++ scripts/collage.js
@@ -0,0 +1,43 @@
+var previousTag;
+
+function init(){
+    fetch_images();
+    $("#load_button").click( function(){
+        if( previousTag != $("#tag_input").val() ){
+            fetch_images();
+        } else{
+            console.log("Tag has not changed since last request");
+        }
+    });
+}
+
+function fetch_images(){
+    tag = $("#tag_input").val();
+    previousTag = tag;
+
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: tag, 
+        sortBy: 'random',
+        links: false,
+        limit: 5,
+        resolution: 'low_resolution',
+        clientId: 'ef0bbd19aa4547dbaca0fa96ef0b30dd',
+        template: '<img src="{{image}}" />',
+        after: rotateImages
+    });
+    feed.run();
+    console.log("fetching images with tag: " + $("#tag_input").val());
+}
+
+function rotateImages(){
+    $('img').each( function(i, img){
+        var rotation = Math.floor((Math.random()*100)-50);
+        var rotation_string = 'rotate(' + rotation + 'deg)';
+
+        img.style.setProperty('border-style', 'solid');
+        img.style.setProperty('transform', rotation_string);
+        img.style.setProperty('-webkit-transform', rotation_string);
+        img.style.setProperty('-ms-transform', rotation_string);
+    });
+}
